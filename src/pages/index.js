fbd8dd88eb8cb663f67babb5a01abae7a4c8cd9b
ffatihsen <52621479+ffatihsen@users.noreply.github.com>
2021-10-25 21:02:00 +0300

    photos added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,47 @@
 import * as React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import "./index.css"
+import { node } from "prop-types";
 //import { Link } from "gatsby"
 //import { StaticImage } from "gatsby-plugin-image"
 
 //import Layout from "../components/layout"
 //import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <div>
-    <h1>Merhaba</h1>
+const IndexPage = ({data}) => {
+  console.log(data);
+  return(
+    <div>
+      <h1>Merhaba</h1>
+      <div className ="photos">
+        {data.allFile.edges.map(({node})=>{
+          return <Img key={node.id} fluid = {node.childImageSharp.fluid}></Img>
+        })}
 
+      </div>
+
+      
   </div>
-)
+  )
+}
+
+export const pageQuery = graphql`
+  {
+  allFile(filter: {absolutePath: {regex: "//photos//"}}) {
+    edges {
+      node {
+        id
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+}
+
+`
 
 export default IndexPage
